ade9547b8801696ca5a096a4a11ff0fee06bcd6a
Aymeric-Thibert <130760614+Aymeric-Thibert@users.noreply.github.com>
2025-08-28 15:26:35 -0400

    debut

---
+++ italy/android/app/src/main/kotlin/com/example/italy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.italy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ oiujioj/android/app/src/main/kotlin/com/example/oiujioj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.oiujioj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
